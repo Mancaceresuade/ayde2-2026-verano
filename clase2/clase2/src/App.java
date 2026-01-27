e7eae8321a0446bd524dfcc355f2e500d252d864
Mancaceresuade <mancaceres@uade.edu.ar>
2026-01-27 19:58:56 -0300

    clase2.2

--- clase2/clase2/src/App.java
+++ clase2/clase2/src/App.java
@@ -4,6 +4,18 @@ public class App {
         System.out.println("hay pares " + hayPares(numeros));
         int[] numeros1 = {8,10,12};
         System.out.println("todos pares " + todosPares(numeros1));
+        int[][] aux2 = {
+                { 9, 12, 18 },
+                { 14, 10, 30 },
+                { 9, 6, 3 } };
+        System.out.println("alguna fila todos multiplos de 2 " + algunaFilaTodosMultiplosDeDos(aux2));        
+
+    private static boolean algunaFilaTodosMultiplosDeDos(int[][] aux2) {
+        boolean rta = false;
+        for (int i = 0; i < aux2.length; i++) {
+            rta = rta || todosPares(aux2[i]);
+        }
+        return rta;
     }
 
     private static boolean todosPares(int[] arr) {
@@ -13,7 +25,7 @@ public class App {
         }
         return rta;
     }
-    
+
     private static boolean hayPares(int[] numeros) {
         boolean rta = false;
         for (int i = 0; i < numeros.length; i++) {
