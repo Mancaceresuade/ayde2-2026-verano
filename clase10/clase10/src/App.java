37464cac2f4b1558e21c4d86510035526dc37f11
Mancaceresuade <mancaceres@uade.edu.ar>
2026-02-10 19:51:15 -0300

    clase 10 - recursividad - arboles

---
+++ clase10/clase10/src/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println(App.factorialRec(5)); // 5*4*3*2*1 = 120
+        int[] numeros = {4,5,6,7}; // 22
+        System.out.println("Suma recursiva "+ sumaRec2(numeros,numeros.length-1));
+    }
+
+    private static int sumaRec(int[] numeros, int i) {
+        if(numeros.length == i) return 0;
+        return numeros[i] + sumaRec(numeros,i+1);
+    }
+
+    private static int sumaRec2(int[] numeros, int i) {
+        if(i == 0) return numeros[i];
+        return numeros[i] + sumaRec2(numeros,i-1);
+    }
+
+    public static int factorialRec(int n) {
+        // caso base
+        if(n==0) return 1;
+        // llamado recursivo
+        int aux = n * factorialRec(n-1);
+        return aux;
+    }  
+
+    public static int factorialIter(int n) {
+        int ret = n;
+        for (int i = n - 1; i > 1; i--) {
+            ret = ret * i;
+        }
+        return ret;
+    }
+}
